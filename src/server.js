2ef458869b0e2df98fea307ff23d957fd9ccb98b
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-11-04 14:39:18 +0700

    complete backend

--- src/server.js
+++ src/server.js
@@ -4,9 +4,13 @@ import cors from 'cors';
 import 'dotenv/config';
 import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
+
 import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
 import { inngest, functions } from "./lib/inngest.js";
+
+import chatRoutes from './routes/chatRoutes.js';
+import sessionRoutes from './routes/sessionRoutes.js';
 
 const app = express();
 const __dirname = path.resolve();
@@ -18,6 +22,8 @@ app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(clerkMiddleware());
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello from the backend server!');
